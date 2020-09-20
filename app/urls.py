890d16a2a5a22f338f8f1bd6d6ad454c524ef29c
jermaine maragh <jermainemaragh@gmail.com>
2020-09-20 00:08:59 -0700

    parcel support added.

--- app/urls.py
+++ app/urls.py
@@ -15,9 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import views
+from django.contrib.auth import views, urls
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
+    # path('admin/', admin.site.urls),
+
+    path('login/', views.LoginView.as_view(template_name='base.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+
+    path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
