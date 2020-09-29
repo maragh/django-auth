e436429a75a7b98e1d06bd3719bba6f6c76ce448
jermaine maragh <jermainemaragh@gmail.com>
2020-09-28 20:43:03 -0700

    basic layout added

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'portfolio',
     'django_auth',
 
     'widget_tweaks',
--- app/urls.py
+++ app/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views, urls
-
-from .views import  BaseView
 
 
 urlpatterns = [
-    path('', BaseView.as_view(), name='base'),
+    path('', include('portfolio.urls', namespace='portfolio')),
 
     path('login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
